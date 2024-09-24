dab87a428b9358cd330c61a2b90094630143fa10
Sarah Threewits <sarah3wits@gmail.com>
2024-09-24 17:45:36 -0400

    Initial Commit

---
+++ ios_bootcamp_spotify/ios_bootcamp_spotifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_bootcamp_spotifyApp.swift
+//  ios_bootcamp_spotify
+//
+//  Created by Sarah Threewits on 9/24/24.
+//
+
+import SwiftUI
+
+@main
+struct ios_bootcamp_spotifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
